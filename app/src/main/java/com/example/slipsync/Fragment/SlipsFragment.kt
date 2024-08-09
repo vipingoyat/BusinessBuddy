715d6729b013ed5da94450ecfa93b221d60a150d
vipingoyat <e21cseu0694@bennett.edu.in>
2024-08-09 15:35:59 +0530

    Slip details is now visible when you touch on any slip

--- app/src/main/java/com/example/slipsync/Fragment/SlipsFragment.kt
+++ app/src/main/java/com/example/slipsync/Fragment/SlipsFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.slipsync.CreatedSlipsAdpater
+import com.example.slipsync.Adapter.CreatedSlipsAdpater
 import com.example.slipsync.databinding.FragmentSlipsBinding
 import com.example.slipsync.model.SlipDetails
 import com.google.firebase.auth.FirebaseAuth
@@ -17,19 +17,16 @@ import com.google.firebase.database.ValueEventListener
 
 class SlipsFragment : Fragment() {
 
-    private var listOfOrderItem: ArrayList<SlipDetails> = arrayListOf()
+    private var listOfSlipItem: ArrayList<SlipDetails> = arrayListOf()
     private lateinit var database: FirebaseDatabase
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: FragmentSlipsBinding
     private lateinit var userId: String
-    private lateinit var createdSlipAdapter:CreatedSlipsAdpater
-
+    private lateinit var createdSlipAdapter: CreatedSlipsAdpater
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -37,7 +34,7 @@ class SlipsFragment : Fragment() {
         auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance()
 
-        retrieveBuyHistory()
+        retrieveSlipHistory()
         binding = FragmentSlipsBinding.inflate(inflater,container, false)
         // Inflate the layout for this fragment
         return binding.root
@@ -48,8 +45,8 @@ class SlipsFragment : Fragment() {
     }
 
 
-    private fun retrieveBuyHistory() {
-        userId = auth.currentUser?.uid ?: ""
+    private fun retrieveSlipHistory() {
+        userId = auth.currentUser?.uid?:""
         val slipRef = database.reference.child("user").child(userId).child("CreatedSlips")
         val shortingQuery = slipRef.orderByChild("slipNumber")
         shortingQuery.addListenerForSingleValueEvent(object : ValueEventListener {
@@ -57,11 +54,11 @@ class SlipsFragment : Fragment() {
                 for (buySnapshot in snapshot.children) {
                     val slipItems = buySnapshot.getValue(SlipDetails::class.java)
                     slipItems?.let {
-                        listOfOrderItem.add(it)
+                        listOfSlipItem.add(it)
                     }
                 }
-                listOfOrderItem.reverse()
-                if (listOfOrderItem.isNotEmpty()) {
+                listOfSlipItem.reverse()
+                if (listOfSlipItem.isNotEmpty()) {
                     //setup the recycler view
                     setPreviousBuyItemRecyclerView()
                 }
@@ -74,22 +71,17 @@ class SlipsFragment : Fragment() {
 
 
     private fun setPreviousBuyItemRecyclerView(){
-        val SlipItemName = mutableListOf<String>()
-        val SlipName = mutableListOf<String>()
-        val SlipItemAmount = mutableListOf<String>()
-        for (i in 0 until listOfOrderItem.size) {
-            listOfOrderItem[i].slipItem?.let { SlipItemName.add(it) }
-            listOfOrderItem[i].slipName?.let { SlipName.add(it) }
-            listOfOrderItem[i].slipAmount?.let { SlipItemAmount.add(it) }
+//        val SlipItemName = mutableListOf<String>()
+//        val SlipName = mutableListOf<String>()
+//        val SlipItemAmount = mutableListOf<String>()
+//        for (i in 0 until listOfSlipItem.size) {
+//            listOfSlipItem[i].slipItem?.let { SlipItemName.add(it) }
+//            listOfSlipItem[i].slipName?.let { SlipName.add(it) }
+//            listOfSlipItem[i].slipAmount?.let { SlipItemAmount.add(it) }
             val rv = binding.slipsRecyclerView
             rv.layoutManager = LinearLayoutManager(requireContext())
-            createdSlipAdapter = CreatedSlipsAdpater(
-                SlipItemName,
-                SlipName,
-                SlipItemAmount,
-                requireContext()
-            )
+            createdSlipAdapter = CreatedSlipsAdpater(listOfSlipItem,requireContext())
             rv.adapter= createdSlipAdapter
-        }
+
     }
 }
--- app/src/main/java/com/example/slipsync/MainActivity.kt
+++ app/src/main/java/com/example/slipsync/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.slipsync
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
